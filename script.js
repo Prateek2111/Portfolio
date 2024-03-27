e47d120062098a955e4b4ca55bf47b598a0eb7e6
Prateek Khare <135314388+Prateek2111@users.noreply.github.com>
2024-03-27 21:03:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let typing1 = document.querySelector(".typing-1");
+let typed1 = new Typed(".typing-1", {
+    strings: ["A Passionate Web Developer","Javascript Developer","React Js Developer","Btech First Year Student in MMMUT GORAKHPUR"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+});
